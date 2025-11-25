e0d451a0a7041347483bb66c37caccd38afb4eb7
Luis <luisfofitochuqimia@gmail.com>
2025-11-24 21:55:48 -0400

    refactor(application): Reestructuramos para la conexion a la base de datos - Realizar la prueba deconexion a la base de datos y el funcionamiento del proyecto

---
+++ src/test/java/com/luis_andres/Mi_web/MiWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luis_andres.Mi_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
